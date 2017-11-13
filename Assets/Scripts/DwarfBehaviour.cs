27b0b6dbcf0ec8f9589c96132a263c0adc91257a
Jean <jean.b4sset@gmail.com>
2017-11-13 16:10:28 +0100

    Terrain

---
+++ Assets/Scripts/DwarfBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DwarfBehaviour : MonoBehaviour {
+
+    public Transform Target;
+
+    private NavMeshAgent agent;
+    private float normalSpeed;
+    private float roadSpeed;
+
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        normalSpeed = agent.speed;
+        roadSpeed = normalSpeed * 2;
+	}
+	
+	void Update ()
+    {
+        agent.SetDestination(Target.position);
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Road"))
+        {
+            agent.speed = roadSpeed;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Road"))
+        {
+            agent.speed = normalSpeed;
+        }
+    }
+}
