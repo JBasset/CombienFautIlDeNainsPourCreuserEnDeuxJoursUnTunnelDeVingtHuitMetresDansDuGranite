14a313a1d86888875c34a9623a0c72783651222c
Aline Roc <aline.roc@ensc.fr>
2017-12-19 16:22:57 +0100

    minor changes

--- Assets/Scripts/VariableStorage.cs
+++ Assets/Scripts/VariableStorage.cs
@@ -29,7 +29,11 @@ namespace Assets.Scripts
         public int TotalGoldMined = 0;
         public int TotalBeerDrank = 0;
 
-        public int oreSpawnRate = 1; // This simulation takes place in a world where gold "appears" continuously in mines. We neglect the economical impact of that property in the simulation's universe. Even though it's worthless, dwarves love gold.
+        public int oreSpawnRate = 1;
+        /* This simulation takes place in a world where gold "appears" continuously in mines. 
+         * We neglect the economical impact of that property in the simulation's universe. 
+         * Even though it's worthless, dwarves love gold. */
+
         public int dwarfOreMiningRate = 10;
 
         public double attenuateTimeImpact = 1; /* this value (0 <= x <= 1) affects a dwarf's chance to rethink his activity */
