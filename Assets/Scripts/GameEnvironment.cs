89757c94f66729c05684a882c25dddeb9994c7c5
Jean <jean.b4sset@gmail.com>
2017-12-19 16:48:54 +0100

    Mine positions adapted for dwarves lines of sights

--- Assets/Scripts/GameEnvironment.cs
+++ Assets/Scripts/GameEnvironment.cs
@@ -34,7 +34,7 @@ namespace Assets.Scripts
             gameEnvironment = GetComponent<Transform>();
             dwarves = gameEnvironment.FindChild("Dwarves");
             mines = gameEnvironment.FindChild("World").FindChild("Mines");
-            spawnsLeft = 20;
+            spawnsLeft = 0;
 
             Transform GeneralStatsPanel = UI.transform.FindChild("GeneralStats");
             timeSinceStart = GeneralStatsPanel.FindChild("TimeSinceStart").FindChild("Value").GetComponent<Text>();
